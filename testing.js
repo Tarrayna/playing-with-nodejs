709bc5def6a8bb84ab13fd509840a631d134d192
tarrayna <tarraynamg15@gmail.com>
2020-11-03 09:41:35 -0800

    Initial Commit

---
+++ testing.js
@@ -0,0 +1,16 @@
+const value = 100;
+const value2 = 101;
+
+
+switch (value)
+{
+    case 100 && value2 ===101:
+        console.log("Case 1")
+        break;
+    case 101:
+        console.log("Case 2")
+        break;
+    default:
+        console.log("Default");
+        break;
+}
